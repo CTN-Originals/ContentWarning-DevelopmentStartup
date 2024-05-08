d6bbc19995ef4d86059e66ee55c9433d4c99545a
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2024-05-08 21:49:44 +0100

    lol

--- DevelopmentStartup/Config.cs
+++ DevelopmentStartup/Config.cs
@@ -1,6 +1,7 @@
+using System;
 using BepInEx;
 using BepInEx.Configuration;
-
+using BepInEx.Logging;
 using CTNOriginals.ContentWarning.DevelopmentStartup.Utilities;
 
 namespace CTNOriginals.ContentWarning.DevelopmentStartup;
@@ -8,12 +9,25 @@ namespace CTNOriginals.ContentWarning.DevelopmentStartup;
 public class Config {
 	public static ConfigFile Configurations = new ConfigFile(Paths.ConfigPath + "\\" + Plugin.PluginGUID + ".cfg", true);
 
+	public enum MapName {
+		Random,
+		Factory,
+		Harbour,
+		Mines
+	}
+	
 	public static ConfigEntry<string> SaveGameIndex;
+	public static ConfigEntry<bool> OldWorldModal;
+	public static ConfigEntry<MapName> MapToTeleportTo;
 
 	public void LoadFile() {
 		SaveGameIndex = Configurations.Bind("General", "SaveGameIndex", "1", "The number of the saved game to load on startup (a number between 1 and 3)");
+		OldWorldModal = Configurations.Bind("General", "OldWorldModal", true, "Whether or not to show a prompt to teleport to old world with a Camera when spawning in.");
+		MapToTeleportTo = Configurations.Bind("General", "MapToTeleportTo", MapName.Random, "The map to teleport to when teleporting to the old world.");
 		
 		CLogger.LogInfo($"SaveGameIndex: {SaveGameIndex.Value}");
+		CLogger.LogInfo($"OldWorldModal: {OldWorldModal.Value}");
+		CLogger.LogInfo($"MapToTeleportTo: {MapToTeleportTo.Value}");
 
 		Configurations.Save();
 	}
--- DevelopmentStartup/Patches/MainMenuHandlerPatch.cs
+++ DevelopmentStartup/Patches/MainMenuHandlerPatch.cs
@@ -1,21 +1,65 @@
+using System;
 using BepInEx;
 using System.Collections;
-
+using System.Collections.Generic;
+using System.Linq;
 using CTNOriginals.ContentWarning.DevelopmentStartup.Utilities;
+using Photon.Pun;
+using UnityEngine;
+using Zorro.Core;
+using Object = System.Object;
 
 namespace CTNOriginals.ContentWarning.DevelopmentStartup.Patches;
 
 public class MainMenuHandlerPatch {
 	public static void Patch() {
 		On.MainMenuHandler.CheckConnected += CheckConnectedPatch;
+		On.SpawnHandler.SpawnLocalPlayer += (orig, self, state) =>
+		{
+			orig(self, state);
+			if(!PhotonNetwork.IsMasterClient) return;
+			if(!PhotonGameLobbyHandler.IsSurface) return;
+			CLogger.LogInfo("SpawnHandler.SpawnLocalPlayerPatch()");
+			if (!Config.OldWorldModal.Value) return;
+			ModalOption[] options = [
+				new ModalOption("Yes", () =>
+				{
+					if(!Player.localPlayer.TryGetInventory(out PlayerInventory pInv)) return;
+					if(!ItemDatabase.TryGetItemFromID(0x01, out Item item)) return;
+					if(!pInv.TryGetSlotWithItemType(Item.ItemType.Camera, out _)) Item.EquipItem(item);
+					
+					PhotonGameLobbyHandler.Instance.photonView.RPC("RPC_StartTransition", RpcTarget.Others, Array.Empty<object>());
+					RetrievableSingleton<PersistentObjectsHolder>.Instance.FindPersistantSurfaceObjects();
+					RetrievableResourceSingleton<TransitionHandler>.Instance.TransitionToBlack(3f, delegate
+					{
+						var text = Config.MapToTeleportTo.Value switch
+						{
+							Config.MapName.Factory => "Factory",
+							Config.MapName.Harbour => "Harbour",
+							Config.MapName.Mines => "Mines",
+							_ => new List<string> { "FactoryScene", "MinesScene", "HarbourScene" }[
+								SurfaceNetworkHandler.RoomStats.LevelToPlay]
+						};
+						
+						PhotonNetwork.LoadLevel(text);
+					}, 0f);
+				}),
+				new ModalOption("No"),
+				new ModalOption("No\nDon't Show Again", () => Config.OldWorldModal.Value = false)
+			];
+
+			Modal.Show("Teleport to Old World", "Would you like to teleport to the old world?", options);
+		};
 	}
 
 	static readonly string errorLogEnd = $"\nPlease correct this in the config ({Paths.ConfigPath + "\\" + Plugin.PluginGUID + ".cfg"}).";
 	static bool hasConnected = false;
+	
 	private static IEnumerator CheckConnectedPatch(On.MainMenuHandler.orig_CheckConnected orig, MainMenuHandler self) {
 		IEnumerator origEnum = orig(self);
 		while (origEnum.MoveNext()) yield return origEnum.Current;
 		if (!hasConnected) {
+			hasConnected = true;
 			CLogger.LogInfo("MainMenuHandler.CheckConnectedPatch()");
 			if (int.TryParse(Config.SaveGameIndex.Value, out int saveIndex)) {
 				if (saveIndex < 1 || saveIndex > 3) {
@@ -28,7 +72,6 @@ public class MainMenuHandlerPatch {
 				CLogger.LogError($"Config.SaveGameIndex was not able to be parsed to an integer, value: ({Config.SaveGameIndex.Value}).{errorLogEnd}");
 				self.Host(0);
 			}
-			hasConnected = true;
 		}
 	}
 }
